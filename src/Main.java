0d686985eba236ef6ba796289d9e1befe8350ac4
Gayanath Lakmevan Silva <gayanathlakmevansilva@gmail.com>
2024-01-15 22:33:45 +0530

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        QueueArray queue = new QueueArray(5);
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        queue.display();
+
+        queue.dequeue();
+        queue.dequeue();
+        queue.display();
+        System.out.println("Peek: " + queue.peek());
+
     }
 }
--- src/QueueArray.java
+++ src/QueueArray.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class QueueArray {
+public class QueueArray {
+    private int maxSize;
+    private int[] queArray;
+    private int front;
+    private int rear;
+    private int nItems;
+
+    public QueueArray(int maxSize) {
+        this.queArray = new int[maxSize];
+        this.maxSize = maxSize;
+        this.front = 0;
+        this.rear = -1;
+        this.nItems = 0;
+    }
+
+    public boolean isEmpty() {
+        return (nItems == 0);
+    }
+
+    public boolean isFull() {
+        return (this.nItems == this.maxSize);
+    }
+
+    public int size() {
+        return this.nItems;
+    }
+
+    public void enqueue(int item) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+            return;
+        }
+        if (rear == maxSize - 1) {
+            rear = -1;
+        }
+        queArray[++rear] = item;
+        nItems++;
+        System.out.println("Enqueued " + item);
+    }
+
+    public int dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        int temp = this.queArray[front];
+        if (this.front == maxSize - 1) {
+            this.front = 0;
+        } else {
+            this.front++;
+        }
+        nItems--;
+        System.out.println("Dequeued " + temp);
+        return temp;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        return queArray[front];
+    }
+
+    public void display() {
+        int i = this.front;
+        while (i != this.rear) {
+            System.out.print(this.queArray[i] + " ");
+            if (i == maxSize - 1) {
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+        System.out.println(this.queArray[this.rear]);
+        System.out.println();
+    }
 }
